06cafc9eff739373d92d4e1aec8e1123aab6b20f
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2026-08-30 01:58:17 +0900

    refactor: remove legacy copy setup

--- assets/copy/js/server.js
+++
@@ -1,5 +0,0 @@
-// Used by the node.js worker for server-side rendering
-import { getRender } from "live_react/server";
-import components from "../react-components";
-
-export const render = getRender(components);
